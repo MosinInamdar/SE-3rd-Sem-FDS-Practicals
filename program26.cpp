2cbac6c3519f6881d0c856c024f5446e7fb6880a
MosinInamdar <100260513+MosinInamdar@users.noreply.github.com>
2022-11-07 09:41:16 +0530

    Add files via upload

---
+++ program26.cpp
@@ -0,0 +1,119 @@
+/*
+Program 26:
+In any language program mostly syntax error occurs due to unbalancing delimiter such as
+(),{},[]. Write C++ program using stack to check whether given expression is well
+parenthesized or not.
+
+Name: Mosin Inamdar
+Roll No. : 34
+Class:Computer SS
+*/
+
+#include<iostream>
+using namespace std;
+const int MAX = 20;
+class Stack
+{
+    char str[MAX];
+    int top;
+    public:
+    Stack(){
+        top=-1;
+    }
+    void push(char ch);
+    char pop();
+    // char getTop();
+    bool isEmpty();
+    bool isFull();
+    void display();
+    void checkParenthesis();
+};
+bool Stack ::isEmpty()
+{
+    if(top==-1)
+    return 1;
+    else return 0;
+}
+
+bool Stack::isFull()
+{
+    if(top==MAX-1)
+    return 1;
+    else return 0;
+}
+
+void Stack::display()
+{
+    if(isEmpty()==1)
+     cout<<"\n Stack is Empty";
+     else{
+         for(int i=0 ; i<=top;i++)
+         {
+             cout<<""<<str[i];
+         }
+     }
+}
+
+void Stack::push(char ch)
+{
+    if(!isFull())
+    {
+        top++;
+        str[top]=ch;
+    }
+}
+
+char Stack::pop()
+{
+    if(!isEmpty())
+    {
+        char ch = str[top];
+        top--;
+        return ch;
+    }
+    else
+    {
+        return '\0';
+    }
+}
+
+void Stack::checkParenthesis()
+{
+    cout<<"\n Enter # as deliminator after expression(At the end)\n";           
+    cout<<"\n Enter Expression : ";
+    cin.getline(str,MAX,'#');
+    char ch;
+    bool flag=0;
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]=='('||str[i]=='['||str[i]=='{')
+        push(str[i]);
+        if(str[i]==')'||str[i]==']'||str[i]=='}')
+        {
+            ch=pop();
+            if((str[i]==')'&& ch!='(')||(str[i]==']'&& ch!='[')||(str[i]=='}'&& ch!='{'))
+            {
+                cout<<"\n Not parenthesized At "<<i<<"="<<str[i];
+                flag=1;
+                break;
+            }
+        }
+    }
+    if(isEmpty()==1 && flag==0)
+    cout<<"\n Expression is well parenthesized.";
+    else
+    cout<<"\n Expression is not well parenthesized.";
+}
+
+int main()
+{
+    int choice;
+    do{
+        Stack s;
+        s.checkParenthesis();
+        cout<<"\n Do you want to continue?{1/0}";
+        cin>>choice;
+
+    }while(choice!=0);
+    return 0;
+}

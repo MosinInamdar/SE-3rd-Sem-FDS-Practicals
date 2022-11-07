1fbe1f5b5810540e4ea759ae8026f57658d6fc43
MosinInamdar <100260513+MosinInamdar@users.noreply.github.com>
2022-11-07 09:48:48 +0530

    Update gBprogram2.py

--- gBprogram2.py
+++ gBprogram2.py
@@ -1,7 +1,11 @@
 '''
+Program 11 (B):
     Write a python program to store roll numbers of student array who attended training program in sorted
     order.Write function for searching whether particular student attended training program or not, 
     using Binary search and Fibonacci search.
+Name : Mosin Inamdar
+Roll No. : 34
+Class : Comp SS
 '''
 l=[]
 def get():
